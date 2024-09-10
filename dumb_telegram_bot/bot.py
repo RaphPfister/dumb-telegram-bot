9ef390ec9b5c242dca5e2125ebea2c223ba82986
RaphPfister <rapfister@protonmail.com>
2024-09-10 17:39:41 +0200

    refacto: cleaner way to pass arguments in the route creation + create cli

--- dumb_telegram_bot/bot.py
+++ dumb_telegram_bot/bot.py
@@ -13,44 +13,47 @@ from dumb_telegram_bot.chains import UserRetrieverRagChain, MimickingChatBot
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger()
 
-bot = telebot.TeleBot(os.environ["TELEGRAM_API_TOKEN"])
-LOGGER.info(bot.__str__)
-DATABASE = LocalPandasDatabase(path="./data/result.json")
-TELEGRAM_USER_LIST = DATABASE.get_unique_users()
-
-RETRIEVER = UserRetrieverRagChain(list_users=TELEGRAM_USER_LIST)
-CHATBOT = MimickingChatBot()
 
 
-@bot.message_handler(commands=["imite", "mimic"])
-def mimic(message):
-    """
-    Syntax: /imite <user> <theme>
+def create_bot_routes(telegram_json_history: str, n_samples: int = 500):
     
-    :param message: User message
-    :return: None
-    """
+    bot = telebot.TeleBot(os.environ["TELEGRAM_API_TOKEN"])
     
-    LOGGER.debug(message.text)
+    DATABASE = LocalPandasDatabase(path=telegram_json_history)
+    TELEGRAM_USER_LIST = DATABASE.get_unique_users()
 
-    message.text.split(' ')
-    print(message.text)
-    split_message = message.text.split(' ')
+    RETRIEVER = UserRetrieverRagChain(list_users=TELEGRAM_USER_LIST)
+    CHATBOT = MimickingChatBot()
 
-    if len(split_message)<2:
-        bot.reply_to(message, "Incorrect syntax.\nHelp: /imite <user> <theme>")
-        return
+    @bot.message_handler(commands=["imite", "mimic"])
+    def mimic(message):
+        """
+        Syntax: /imite <user> <theme>
+        
+        :param message: User message
+        :return: None
+        """
+        
+        LOGGER.debug(message.text)
 
-    user = split_message[1]
-    theme = split_message[2:]
+        message.text.split(' ')
+        print(message.text)
+        split_message = message.text.split(' ')
 
-    print(f"Queried user: {user}, theme: {theme}")
-    telegram_username = RETRIEVER.get_telegram_username(user=user)
-    print(f"Found username: {telegram_username}")
-    text_samples = DATABASE.get_sample_from_user(n_samples=300, username=telegram_username)
-    
-    answer = CHATBOT.get_message(text_samples=text_samples, person_to_mimic=telegram_username, theme=theme)
+        if len(split_message)<2:
+            bot.reply_to(message, "Incorrect syntax.\nHelp: /imite <user> <theme>")
+            return
 
-    bot.reply_to(message=message, text=answer)
+        user = split_message[1]
+        theme = split_message[2:]
 
-bot.infinity_polling()
+        print(f"Queried user: {user}, theme: {theme}")
+        telegram_username = RETRIEVER.get_telegram_username(user=user)
+        print(f"Found username: {telegram_username}")
+        text_samples = DATABASE.get_sample_from_user(n_samples=n_samples, username=telegram_username)
+        
+        answer = CHATBOT.get_message(text_samples=text_samples, person_to_mimic=telegram_username, theme=theme)
+
+        bot.reply_to(message=message, text=answer)
+
+    return bot
